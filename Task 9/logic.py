e1597e41228352b26a1431eb6bccf0412a55a46c
Mariia Polishchuk <94909216+mariiapol@users.noreply.github.com>
2024-01-19 16:15:02 +0000

    Add files via upload

---
+++ Task 9/logic.py
@@ -0,0 +1,10 @@
+# find average of 2 numbers
+a = float(input("Input a number:"))
+b = float(input("Input another number:"))
+
+print("Average of numbers is: ")
+
+#the logic error
+average = a + b / 2
+
+print(average)
